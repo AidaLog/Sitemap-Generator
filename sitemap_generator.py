469d33e7e94b1e8c57ba34b283635a34c1a6e121
eddygulled <edgargulay69@gmail.com>
2023-09-21 12:39:18 +0300

    ready to ship 🚀🚀

--- sitemap_generator.py
+++ sitemap_generator.py
@@ -2,43 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 from xml.etree.ElementTree import Element, SubElement, ElementTree
 
-# Function to crawl a webpage and extract links
 def crawl_page(url):
+    """
+    # Function to crawl a webpage and extract links
+    :param url::  str: url of the webpage
+    :return:: []:list
+    """
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         links = []
-
         for link in soup.find_all('a'):
             href = link.get('href')
             if href and href.startswith('http'):
                 links.append(href)
-
         return links
     except Exception as e:
         print(f"Error crawling {url}: {e}")
         return []
 
-# Function to generate a sitemap XML
+
 def generate_sitemap(start_url):
+    """
+    # Function to generate a sitemap
+    :param start_url:: str: url of the website
+    :return:: None
+    """
     root = Element('urlset')
     root.set('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
-
-    # Crawl the initial page
     links_to_crawl = [start_url]
-
     while links_to_crawl:
         url = links_to_crawl.pop()
         links = crawl_page(url)
-
         for link in links:
             if link not in links_to_crawl:
                 links_to_crawl.append(link)
-
             url_element = SubElement(root, 'url')
             loc_element = SubElement(url_element, 'loc')
             loc_element.text = link
-
     tree = ElementTree(root)
     tree.write('sitemap.xml')
 
